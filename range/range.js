46370d5fcee2ecf5f60e247ec0ddd3cea1e5c816
kalme <kalme0714@gmail.com>
2026-04-14 19:13:31 +0900

    add: ベースファイルの作成

---
+++ range/range.js
@@ -0,0 +1,10 @@
+/**
+ *  Made by kalme-egg
+ *  version: 0.0.0
+ * 
+ *  CC-BY 4.0
+ *  https://creativecommons.org/licenses/by/4.0/
+ * 
+ *  You should read "readme.md" before you use this module.
+ * 
+ */
---
+++ range/readme.md
@@ -0,0 +1,10 @@
+# 数値範囲module
+## 更新履歴
+
+## ライセンス
+CC-BY 4.0にて配布しています。  
+https://creativecommons.org/licenses/by/4.0/  
+(このライセンスは他のモジュールに適用されない可能性があります)
+## 使用方法
+
+## docs
